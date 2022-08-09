2d49a014f56d95841765c8ea652a62085f017657
Eze Chiemerie <ezehdavid960@outlook.com>
2022-08-09 09:45:45 +0100

    commit

--- js/app.js
+++ js/app.js
@@ -3,26 +3,29 @@ let cardHr = document.querySelector('.cardHr')
 let cardMnth = document.querySelector('.mnth')
 let cardYr = document.querySelector('.yr')
 let cvc = document.querySelector('.cvc-hr')
-let x = document.querySelector('.card-num')
-let y = document.querySelector('.card-hr')
-let a = document.querySelector('.mm')
-let b = document.querySelector('.yy')
-let c = document.querySelector('.cvc')
+
 cardNr.addEventListener('input', () => {
   function format(s) {
-  return s.toString().replace(/\d{4}(?=.)/g, '$& ');
-}
-x.innerHTML = format(cardNr.value)
+    return s.toString().replace(/\d{4}(?=.)/g, '$& ');
+  }
+  document.querySelector('.card-num').innerHTML = format(cardNr.value)
 })
 cardHr.addEventListener('input', () => {
-  y.innerHTML = cardHr.value
+  document.querySelector('.card-hr').innerHTML = cardHr.value
 })
-cardMnth.addEventListener('input', ()=> {
-  a.innerHTML = cardMnth.value
+cardMnth.addEventListener('input', () => {
+  document.querySelector('.mm').innerHTML = cardMnth.value
 })
-cardYr.addEventListener('input', ()=> {
-  b.innerHTML = cardYr.value
+cardYr.addEventListener('input', () => {
+  document.querySelector('.yy').innerHTML = cardYr.value
 })
-cvc.addEventListener('input', ()=> {
-  c.innerHTML = cvc.value
+cvc.addEventListener('input', () => {
+  document.querySelector('.cvc').innerHTML = cvc.value
 })
+
+
+
+
+
+
+
